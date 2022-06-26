62eadd323444a3c1b2cb0566c476df77878cdbd9
Rae7771653 <nicole831220@gmail.com>
2022-06-26 19:02:38 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//伺服器設定
+const express = require("express")
+const app = express()
+const port = 3000
+
+//樣板引擎建立
+const exphbs = require("express-handlebars")
+app.engine('handlebars', exphbs({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+
+//連接靜態檔案
+app.use(express.static('public'))
+
+//顯示動態資料
+const data = require('./restaurant.json')
+
+// 1.顯示首頁清單
+app.get('/',(req, res) =>{
+  res.render('index',{restaurants:data.results})
+})
+// 2.顯示點選的餐廳詳細資訊
+app.get('/restaurants/:id',(req, res) =>{
+  const onclickRestaurant = data.results.find(d => d.id.toString() === req.params.id)
+  res.render('show', {restaurant:onclickRestaurant})
+})
+// 3.顯示符合搜尋關鍵字的餐廳清單
+app.get('/search',(req, res) =>{
+  const keyword = req.query.keyword.toLowerCase()
+  const searchRestaurant = data.results.filter(
+    d => {
+      return d.name.toLowerCase().includes(keyword) || d.name_en.toLowerCase().includes(keyword) || d.category.toLowerCase().includes(keyword)
+  })
+    res.render('index', { restaurants: searchRestaurant })
+})
+
+//伺服器監聽器
+app.listen(port, () =>{
+  console.log(`It is running on http://localhost:${port}`)
+})
