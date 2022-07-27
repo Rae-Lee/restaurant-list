2ff70ace14d2335997847dbc8ccd0ba592b53e79
Rae7771653 <nicole831220@gmail.com>
2022-07-27 20:38:26 +0800

    feat:add restaurantList seeder

--- models/restaurant-list.js
+++ models/restaurant-list.js
@@ -1,10 +1,37 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantListSchema = new Schema({
+  id:{ type: Number },
   name:{
-    type:string,
+    type: String,
+    required: true
+  },
+  name_en:{ type: String },
+  category:{ type: String },
+  image:{
+    type: String,
     required:true
-  }
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength:12,
+    maxLength:12
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min:0,
+    max:5
+  },
+  description: { type: String }
 })
 
 module.exports = mongoose.model('RestaurantList', restaurantListSchema) 
