e905e240a059e348e3ff1ef898c5681158164787
Rae7771653 <nicole831220@gmail.com>
2022-08-07 23:00:24 +0800

    feat:model restaurant_list add all kinds of category params.

--- models/restaurant-list.js
+++ models/restaurant-list.js
@@ -8,7 +8,8 @@ const restaurantListSchema = new Schema({
   },
   name_en:{ type: String },
   category:{ 
-    type: String
+    type: String,
+    enum: ['美式', '咖啡', '中東料理', '日本料理', '義式餐廳', '酒吧', '其他' ]
   },
   image:{
     type: String,
@@ -31,7 +32,35 @@ const restaurantListSchema = new Schema({
     min:0,
     max:5
   },
-  description: { type: String }
+  description: { type: String },
+  American: {
+    type: Boolean,
+    default:false
+  },
+  cafe: {
+    type: Boolean,
+    default: false
+  },
+  MiddleEastern: {
+    type: Boolean,
+    default: false
+  },
+  Japanese: {
+    type: Boolean,
+    default: false
+  },
+  pub: {
+    type: Boolean,
+    default: false
+  },
+  Italian: {
+    type: Boolean,
+    default: false
+  },
+  other: {
+    type: Boolean,
+    default: false
+  }
 })
 
 module.exports = mongoose.model('RestaurantList', restaurantListSchema) 
--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -13,6 +13,14 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
   data.forEach(d => {
+    d.American = d.category === '美式'
+    d.cafe = d.category === '咖啡'
+    d.MiddleEastern = d.category === '中東料理'
+    d.Japanese = d.category === '日本料理'
+    d.Italian = d.category === '義式餐廳'
+    d.pub = d.category === '酒吧'
+    d.other = d.category === '其他'
+
     RestaurantList.create(d)
   })
 })
